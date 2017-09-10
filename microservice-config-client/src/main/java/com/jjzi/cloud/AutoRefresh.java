063af48b86bab1b4250d6edc6f4f3ad7e92e61d6
zjjfly <zjjblue@gmail.com>
2017-09-10 09:28:19 +0800

    加入config server和client

---
+++ microservice-config-client/src/main/java/com/jjzi/cloud/AutoRefresh.java
@@ -0,0 +1,19 @@
+package com.jjzi.cloud;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.endpoint.RefreshEndpoint;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+//自动刷新配置
+@Configuration
+@EnableScheduling
+public class AutoRefresh {
+    @Autowired
+    private RefreshEndpoint refreshEndpoint;
+    @Scheduled(cron = "0 * * * * *")
+    public void refresh(){
+        refreshEndpoint.refresh();
+    }
+}
