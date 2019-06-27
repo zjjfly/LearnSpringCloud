701c29604b7822ecc334a31331338a5d58ee8d77
zjjfly <zjjblue@gmail.com>
2019-06-27 23:25:16 +0800

    升级gradle

--- microservice-provider/src/main/java/com/jjzi/cloud/UserApplication.java
+++ microservice-provider/src/main/java/com/jjzi/cloud/UserApplication.java
@@ -2,10 +2,12 @@ package com.jjzi.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EntityScan("com.jjzi.*")
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class,args);
